fcb018298bf2e231e1d9de7571a141cd9f33c7a1
alex <alex.titakoff@yandex.ru>
2017-11-24 20:02:01 +0300

    music

---
+++ env/README.md
@@ -0,0 +1,5 @@
+Библеотка
+https://github.com/eternnoir/pyTelegramBotAPI/
+
+Рус дока:
+https://groosha.gitbooks.io/telegram-bot-lessons/content/chapter1.html
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import telebot
-import constants
+import config
+import os
+from time import sleep
+import time
+
 import requests
 
-bot = telebot.TeleBot(constants.token)
+bot = telebot.TeleBot(config.token)
+
+# echo bot function   /  функция эхо
+# @bot.message_handler(content_types=["text"])
+# def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+#     bot.send_message(message.chat.id, message.text)
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+
+@bot.message_handler(commands=['test'])
+def find_file_ids(message):
+    for file in os.listdir('music/'):
+        if file.split('.')[-1] == 'ogg':
+            f = open('music/'+file, 'rb')
+            print(f)
+            msg = bot.send_voice(message.chat.id, f, None)
+            # А теперь отправим вслед за файлом его file_id  (* msg.voice.file_id)
+            bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
+        time.sleep(3)
+
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
+
+
+
+
+
+
+
+
+
 
 
 # работаяюща функции из предыдущего лессона
